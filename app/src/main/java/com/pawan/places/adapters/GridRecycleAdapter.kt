770f511f0637a7a86d24d220b38d9011a4748724
Pawan Ghewande <ghewandepawan@gmail.com>
2021-05-02 12:29:01 +0530

    improved image placeholders

--- app/src/main/java/com/pawan/places/adapters/GridRecycleAdapter.kt
+++ app/src/main/java/com/pawan/places/adapters/GridRecycleAdapter.kt
@@ -1,13 +1,15 @@
 package com.pawan.places.adapters
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.Priority
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 import com.pawan.places.R
 import com.pawan.places.db.model.ImageData
@@ -32,9 +34,15 @@ class GridRecycleAdapter(private val c: Context, private val images: List<ImageD
     override fun onBindViewHolder(holder: ColorViewHolder, position: Int) {
         val path = images.get(position)
 
+        val requestOption = RequestOptions()
+            .placeholder(R.drawable.ic_placeholder).fitCenter()
+
         Glide.with(c)
             .load(path.download_url)
-            .placeholder(R.drawable.ic_placeholder)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .thumbnail(Glide.with(c)
+                .load("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.daouidc.com%2Fresources%2Fimages%2Fcommon%2Floading.gif&f=1&nofb=1")
+                .apply(requestOption))
             .into(holder.imageView)
 
         holder.imageView.setOnClickListener {
